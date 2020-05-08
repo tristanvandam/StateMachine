d516ba411847578825d2376b8892e7d1461876d8
TristanVanDam <tristan.vandam@entelect.co.za>
2020-05-08 10:32:51 +0200

    Started OnEnter and EnExit Methods for Latches

--- StateMachine.Core/FiniteStateMachine.cs
+++ StateMachine.Core/FiniteStateMachine.cs
@@ -1,27 +1,41 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace StateMachine.Core
 {
     public class FiniteStateMachine : IFiniteStateMachine
     {
         private Latch _currentLatch;
+        private readonly Action<string> _internalLog;
 
-        public FiniteStateMachine(Latch initialStateLatch)
+        public FiniteStateMachine(Latch initialStateLatch, Action<string> internalLog = null)
         {
+
             _currentLatch = initialStateLatch;
+            _internalLog = internalLog ?? delegate(string s) {  };
 
             // will be implementing this StateMachine: https://miro.medium.com/max/1520/1*z-cGUTYS40cSjcMWgiOLww.png
-
-          
-
         }
 
         public void TriggerAction(Actions actions)
         {
-            Console.WriteLine($"Current latch ({_currentLatch.Name}) with action: '{actions.Name}'");
-            _currentLatch = _currentLatch.Trigger(actions);
-            Console.WriteLine($"Current latch is now {_currentLatch.Name}");
-            Console.WriteLine($"------------------------------------");
+            _internalLog($"Current latch ({_currentLatch.Name}) with action: '{actions.Name}'");
+            var nextLatch  = _currentLatch.NextLatchFromAction(actions);
+            _internalLog($"Next latch ({_currentLatch.Name}) from action: '{actions.Name}'");
+
+            if (nextLatch.GetType() == _currentLatch.GetType() && _currentLatch.DoNothingOnReEnter)
+            {
+                //DO Nothing
+                return;
+            }
+
+            _currentLatch.Exit();
+
+            _currentLatch = nextLatch;
+            _currentLatch.Enter();
+
+            _internalLog($"Current latch is now {_currentLatch.Name}");
+            _internalLog($"------------------------------------");
 
         }
 
--- StateMachine.Core/Latch.cs
+++ StateMachine.Core/Latch.cs
@@ -8,25 +8,41 @@ namespace StateMachine.Core
     {
         private readonly IDictionary<Actions, Func<Latch>> _actions;
 
-        public Latch(State maintainedState, string name = "default")
+        public Latch(State maintainedState, string name = "default", bool doNothingOnReEnter = false)
         {
             _actions = new Dictionary<Actions, Func<Latch>>();
             State = maintainedState;
             Name = name;
+            DoNothingOnReEnter = doNothingOnReEnter;
         }
+
+        public State State { get; }
+        public string Name { get; set; }
+        public bool DoNothingOnReEnter { get; }
+
 
         public void AddActions(IDictionary<Actions, Func<Latch>> actions)
         {
             actions.ToList().ForEach(x => _actions[x.Key] = x.Value);   //Override if new function is defined with same key.
         }
-
-        public State State { get; }
-        public string Name { get; set; }
-
-        public Latch Trigger(Actions actions)
+        internal Latch NextLatchFromAction(Actions actions)
         {
             //If state not found we will return the current state
             return !_actions.TryGetValue(actions, out var latchFactory) ? this : latchFactory.Invoke();
+        }
+
+        internal void Exit()
+        {
+            //TODO make overridable onExit Method
+
+            throw new NotImplementedException();
+        }
+
+
+        internal void Enter()
+        {
+            //TODO make overridable onEnter Method
+            //throw new NotImplementedException();
         }
     }
 }
